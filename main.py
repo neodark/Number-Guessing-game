b0eea7cb03df587f5b4713043014a42046e31052
Flavio Tarsetti <flavio.tarsetti@idiap.ch>
2022-10-05 00:59:48 +0200

    pass of pre-commit cleanup on all files

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-import random
 import math
- # Take Inputs
-lower = int(input("Enter Lower bound :- ")) 
- 
+import random
+
 # Take Inputs
-upper = int(input("Enter Upper bound :- "))  
- 
+lower = int(input("Enter Lower bound :- "))
+
+# Take Inputs
+upper = int(input("Enter Upper bound :- "))
+
 # generating any random number between the lower and upper
 x = random.randint(lower, upper)
-print("\n\tYou've only ", round(math.log(upper - lower + 1, 2))," chances to guess the integer!\n")
- 
- # Initialize number of guesses
+print(
+    "\n\tYou've only ",
+    round(math.log(upper - lower + 1, 2)),
+    " chances to guess the integer!\n",
+)
+
+# Initialize number of guesses
 count = 0
- 
+
 # for calculation of minimum number of guesses depends upon range
 while count < math.log(upper - lower + 1, 2):
     count += 1
-   
+
     # take guessing number as input
-    guess = int(input("Guess a number :- ")) 
-     
+    guess = int(input("Guess a number :- "))
+
     # Condition testing
-    if x == guess:  
-       print("Congratulations you did it in ", count, " tries!")
-       # Once guessed, loop will break 
-       break
+    if x == guess:
+        print("Congratulations you did it in ", count, " tries!")
+        # Once guessed, loop will break
+        break
     elif x > guess:
-       print("You Guessed too Small!")
+        print("You Guessed too Small!")
     elif x < guess:
-       print("You Guessed too High!")
- 
+        print("You Guessed too High!")
+
 # If Guessing is more than required guesses, then shows this output
 if count >= math.log(upper - lower + 1, 2):
-   print("\nThe number is %d"%x)
-   print("\tBetter Luck Next time !")
+    print("\nThe number is %d" % x)
+    print("\tBetter Luck Next time !")
